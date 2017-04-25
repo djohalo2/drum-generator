5ce0dc9e21d9748653fe586f0a2153fb93bd33f2
Djovanni Tehubijuluw <djohalo2@hotmail.com>
2017-04-25 11:06:13 +0200

    Spacing

--- src/constants/samples.js
+++ src/constants/samples.js
@@ -1,13 +1,13 @@
 export default {
-    kick: new Audio("/static/audio/kick.wav"),
-    snare: new Audio("/static/audio/snare.wav"),
-    "hi-hat": new Audio("/static/audio/hi-hat.wav"),
-    "high-tom": new Audio("/static/audio/high-tom.wav"),
-    "mid-tom": new Audio("/static/audio/mid-tom.wav"),
-    "low-tom": new Audio("/static/audio/low-tom.wav"),
-    crash: new Audio("/static/audio/crash.wav"),
-    ride: new Audio("/static/audio/ride.wav"),
-    ghost: new Audio("/static/audio/ghost.wav"),
+    kick:          new Audio("/static/audio/kick.wav"),
+    snare:         new Audio("/static/audio/snare.wav"),
+    "hi-hat":      new Audio("/static/audio/hi-hat.wav"),
+    "high-tom":    new Audio("/static/audio/high-tom.wav"),
+    "mid-tom":     new Audio("/static/audio/mid-tom.wav"),
+    "low-tom":     new Audio("/static/audio/low-tom.wav"),
+    crash:         new Audio("/static/audio/crash.wav"),
+    ride:          new Audio("/static/audio/ride.wav"),
+    ghost:         new Audio("/static/audio/ghost.wav"),
     "hi-hat-open": new Audio("/static/audio/hi-hat-open.wav")
 }
 
